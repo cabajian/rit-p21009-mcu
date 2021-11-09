6f7dbaf81f9af5de653cdc765eeaff4e60d2b681
cabajian <abajianchris@gmail.com>
2021-11-09 00:33:41 -0500

    Refactored hierarchy.

--- s_fsr.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * File    : s_fsr.cpp
- * Author  : Chris Abajian (cxa6282@rit.edu)
- * Created : September 7, 2021
- *
- * Description:
- *   Handles initialization and data retrieval of the
- *   FSR's on the test mannequin's exterior.
- */
